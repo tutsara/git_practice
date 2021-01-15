ffb5d2e43b6d13234c5ccf4b6ed8e6020a991f21
Sara <medetovasara@gmail.com>
2021-01-15 11:32:31 +0600

    initial commit.

---
+++ git_comment.py
@@ -0,0 +1,13 @@
+git init -.git
+git remote add name url(https,ssh) 
+git pull origin master 
+git status
+git add
+git add
+git comeit -n 'comment'
+git branch
+git branch name branch
+git checkout name branch
+git push name branch
+get push origin master
+git resert file name 
